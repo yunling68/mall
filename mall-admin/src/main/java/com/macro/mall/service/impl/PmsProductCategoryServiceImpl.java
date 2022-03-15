dfff9670a42711cb6625b7f515afd3eae4c3cc1d
qiansu.lyl@antfin.com <qiansu.lyl@antfin.com>
2022-03-15 13:55:30 +0800

    init project

--- mall-admin/src/main/java/com/macro/mall/service/impl/PmsProductCategoryServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/PmsProductCategoryServiceImpl.java
@@ -10,10 +10,10 @@ import com.macro.mall.mapper.PmsProductCategoryMapper;
 import com.macro.mall.mapper.PmsProductMapper;
 import com.macro.mall.model.*;
 import com.macro.mall.service.PmsProductCategoryService;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
